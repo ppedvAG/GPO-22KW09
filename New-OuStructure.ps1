9e07b1a2fcf2795b909c27943798303b6a56510a
Stefan Ober <stefan.ober@shitnet.de>
2022-03-01 11:35:02 +0100

    Commit2

---
+++ New-OuStructure.ps1
@@ -0,0 +1,17 @@
+﻿[cmdletBinding()]
+param(
+
+[Parameter(Mandatory=$true)]
+[string[]]$Namen
+)
+
+foreach ($Name in $Namen)
+{
+New-ADOrganizationalUnit -Name:"$Name" -Path:"OU=Struktur,DC=ppedv,DC=test" -ProtectedFromAccidentalDeletion:$true -Server:"Server1.ppedv.test"
+Set-ADObject -Identity:"OU=$Name,OU=Struktur,DC=ppedv,DC=test" -ProtectedFromAccidentalDeletion:$true -Server:"Server1.ppedv.test"
+New-ADOrganizationalUnit -Name:"Computer" -Path:"OU=$Name,OU=Struktur,DC=ppedv,DC=test" -ProtectedFromAccidentalDeletion:$true -Server:"Server1.ppedv.test"
+Set-ADObject -Identity:"OU=Computer,OU=$Name,OU=Struktur,DC=ppedv,DC=test" -ProtectedFromAccidentalDeletion:$true -Server:"Server1.ppedv.test"
+New-ADOrganizationalUnit -Name:"Benutzer" -Path:"OU=$Name,OU=Struktur,DC=ppedv,DC=test" -ProtectedFromAccidentalDeletion:$true -Server:"Server1.ppedv.test"
+Set-ADObject -Identity:"OU=Benutzer,OU=$Name,OU=Struktur,DC=ppedv,DC=test" -ProtectedFromAccidentalDeletion:$true -Server:"Server1.ppedv.test"
+New-ADGroup -GroupCategory:"Security" -GroupScope:"Global" -Name:"GL-$Name" -Path:"OU=$Name,OU=Struktur,DC=ppedv,DC=test" -SamAccountName:"GL-$Name" -Server:"Server1.ppedv.test"
+}
